64a12b5d59d60a9362593ee7e57449d2e81ac9cd
sergio <sergio.g2388@gmail.com>
2023-05-07 23:51:28 -0300

    first commit

---
+++ carpeta.js/Codigo.js
@@ -0,0 +1,11 @@
+/* alert("Buenos días Acabas de entrar en mi pagina");*/
+let a,b,c;
+let cadena1;
+ a = prompt("Ingrese Nombre: ");
+ b = prompt("Ingrese Apellido: ");
+ c = prompt("Ingrese Edad: ");
+
+cadena1 += a;
+cadena1 += b;
+cadena1 += c;
+alert("Datos de usuario: " + a + b + c );  // Mostrará "hola mundo cruel".
